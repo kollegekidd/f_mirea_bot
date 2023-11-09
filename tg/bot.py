b96e7a29fb33ed4dafe33caa9e2e059b06520655
home <so2blackmarket@gmail.com>
2023-11-09 06:33:56 +0300

    aaaaa

--- tg/bot.py
+++ tg/bot.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-# pylint: disable=unused-argument
-# This program is dedicated to the public domain under the CC0 license.
-
-"""Simple inline keyboard bot with multiple CallbackQueryHandlers.
-
-This Bot uses the Application class to handle the bot.
-First, a few callback functions are defined as callback query handler. Then, those functions are
-passed to the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Example of a bot that uses inline keyboard that has multiple CallbackQueryHandlers arranged in a
-ConversationHandler.
-Send /start to initiate the conversation.
-Press Ctrl-C on the command line to stop the bot.
-"""
 import logging
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
@@ -27,26 +11,22 @@ from telegram.ext import (
 from mirea_db.db import Database
 from tg.config import bot_token
 
-# Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-# set higher logging level for httpx to avoid all GET and POST requests being logged
+
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-# Stages
 START_ROUTES, MID_ROUTES, END_ROUTES = range(3)
-# Callback data
+
 ONE = 'first'
 
 db = Database(link="localhost:27017")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Send message on `/start`."""
-    # Get user that sent /start and log his name
     user = update.message.chat_id
     logger.info("User %s started the conversation.", user)
 
@@ -74,7 +54,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 
 
 async def one(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Show new choice of buttons"""
     query = update.callback_query
     await query.answer()
     keyboard = [
@@ -100,7 +79,6 @@ async def one(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 
 
 async def tetetete(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Show new choice of buttons. This is the end point of the conversation."""
     query = update.callback_query
 
     # test_values = db.find_lessons_by_group_and_day("ФКБО-01-23", 0, "Понедельник")
@@ -108,21 +86,17 @@ async def tetetete(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     keyboard = [
         [
             InlineKeyboardButton("Yes, let's do it again!", callback_data=str(ONE)),
-            InlineKeyboardButton("Nah, I've had enough ...", callback_data=str(TWO)),
+            InlineKeyboardButton("Nah, I've had enough ...", callback_data=str(ONE)),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await query.edit_message_text(
         text=str('test_values'), reply_markup=reply_markup
     )
-    # Transfer to conversation state `SECOND`
     return END_ROUTES
 
 
 async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Returns `ConversationHandler.END`, which tells the
-    ConversationHandler that the conversation is over.
-    """
     query = update.callback_query
     user_id = update.message.chat_id
     text = update.callback_query.data
@@ -154,16 +128,8 @@ async def new(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 def main() -> None:
-    """Run the bot."""
-    # Create the Application and pass it your bot's token.
     application = Application.builder().token(bot_token).build()
 
-    # Setup conversation handler with the states FIRST and SECOND
-    # Use the pattern parameter to pass CallbackQueries with specific
-    # data pattern to the corresponding handlers.
-    # ^ means "start of line/string"
-    # $ means "end of line/string"
-    # So ^ABC$ will only allow 'ABC'
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
@@ -178,10 +144,8 @@ def main() -> None:
         fallbacks=[CommandHandler("start", start)],
     )
 
-    # Add ConversationHandler to application that will be used for handling updates
     application.add_handler(conv_handler)
 
-    # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
